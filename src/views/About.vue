38ef88b282a48801c249461f76aba50d5f574575
Geovany Cipriano <geocipriano28@gmail.com>
2021-03-19 00:42:27 -0300

    Criando Projeto

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="d-flex justify-center">Aplicativo para controle de tarefas</h1>
+    <v-btn
+      color="blue-grey"
+      class="ma-2 white--text"
+      v-for="social in socialMedia"
+      :key="social.title"
+      :to="socialMedia.to"
+    >
+      {{ social.title }}
+      <v-icon
+        right
+        dark
+      >
+        {{ social.icon }}
+      </v-icon>
+    </v-btn>
   </div>
 </template>
+<script>
+export default {
+    data: () => ({
+       socialMedia: [
+          { title: 'Instagram', icon: 'mdi-instagram', to: '/' },
+          { title: 'Github', icon: 'mdi-github', to: '/about' },
+        ]
+       }),
+  }
+</script>
+
